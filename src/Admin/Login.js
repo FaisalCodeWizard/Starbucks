15a64ab23f92ed7f814a32031959b606f92b269e
FaisalCodeWizard <faisalfarooqui077@gmail.com>
2024-02-20 22:43:02 +0530

    implement json server

--- src/Admin/Login.js
+++ src/Admin/Login.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import axios from "axios";
 import './Register.css';
 import Footer from '../Reward Components/Footer';
 
 function LoginPage() {
-  return (
+    const[userlogin,setUserlogin]=useState();
+
+    let apiurl="http://localhost:4000/AdminRegister"
+    const inputHandler=(e)=>{
+        setUserlogin({...userlogin,[e.target.name]: e.target.value})
+    }
+
+    const loginuser=(e)=>{
+        e.preventDefault();
+        axios.post(`${apiurl}`,userlogin)
+        .then(()=>{
+            alert("Admin Register")
+            setUserlogin(null);
+            e.target.reset();
+        }).catch(err=>console.log(err))
+        // console.log(userlogin);
+    }
+
+    return (
     <>
     <div className='container-fluid head'>
         <div className='row'>
@@ -15,23 +35,24 @@ function LoginPage() {
         </div>
     </div>
     <div className='container mt-5 inset'>
+    <form onSubmit={loginuser} autoComplete='off'>
         <div className='row justify-content-center'>
             <div className='col-md-5 border bdr'>
                 <h3 align="center" className='my-4'>Create Account</h3>
                 <div className='mb-3'>
-                    <input type='text' placeholder='FullName' className='form-control'/>
+                    <input type='text' placeholder='FullName' className='form-control' name="fname" value={userlogin?.fname} onChange={inputHandler} />
                 </div>
                 <div className='mb-3'>
-                    <input type='email' placeholder='Email' className='form-control'/>
+                    <input type='email' placeholder='Email' className='form-control' name="email" value={userlogin?.email} onChange={inputHandler}/>
                 </div>
                 <div className='mb-3'>
-                    <input type='mobile' placeholder='Mobile' className='form-control'/>
+                    <input type='mobile' placeholder='Mobile' className='form-control' name="mobile" value={userlogin?.mobile} onChange={inputHandler}/>
                 </div>
                 <div className='mb-3'>
-                    <input type='text' placeholder='UserName' className='form-control'/>
+                    <input type='text' placeholder='UserName' className='form-control' name="username" value={userlogin?.username} onChange={inputHandler}/>
                 </div>
                 <div className='mb-3'>
-                    <input type='password' placeholder='Password' className='form-control'/>
+                    <input type='password' placeholder='Password' className='form-control' name="password" value={userlogin?.password} onChange={inputHandler}/>
                 </div>
                 <div className='dc lg'>
                 <button className='btn btn-dark rounded-pill mt-2 mb-3 decor'><b>Sign up</b></button>
@@ -39,6 +60,7 @@ function LoginPage() {
                 <Link to="/Register" className='nav-link mb-3 lg1'><u>Already a user ?</u></Link>
             </div>
         </div>
+        </form>
     </div>
     <hr/>
     <Footer/>
